7b6d265cc270343b4365e1bf0f7823828d7ecb46
Bhavik Singh <bhaviksingh@gmail.com>
2022-05-18 14:40:58 -0700

    Added framer motion

--- components/exerciseCard.tsx
+++ components/exerciseCard.tsx
@@ -1,39 +1,42 @@
 import classNames from "classnames";
 import { useState } from "react";
-import { useSpring, animated, config } from 'react-spring'
 
+import { motion } from "framer-motion";
 
 interface exerciseProps {
   exercise: Exercise;
   share?: boolean;
 }
 
-
-
 const ExerciseCard: React.FunctionComponent<exerciseProps> = ({ exercise, share }) => {
-
   const [flipCard, setflipCard] = useState(false);
 
-  const cardSpringProps = {config:  {mass: 0.1, tension: 200, friction: 5}};
-  const mainProps = useSpring({ to: {opacity: 1}, from: {opacity: 0}})
-  const rotateProps = useSpring({to: { transform: 'rotateY(180deg) scale(1.1)'} , from: { transform: 'rotateY(360deg)'}, ...cardSpringProps})
-  const rotatePropsReverse = useSpring({to: { transform: 'rotateY(0deg) scale(1.1)'} , from: { transform: 'rotateY(180deg)'}, ...cardSpringProps})
-  
   return (
-    <animated.div className="tarotCard" onClick={ e => setflipCard(!flipCard)} >
-      <animated.div className={'contentContainer padded stack border front'}  style={flipCard ? rotateProps : {}}>
-          <div className="caption"> {exercise.name} </div>
-          <div className="flex-1"> {exercise.text} </div>
-          <div className="align-end horizontal-stack fullWidth">
-            <span> {ratingRenderer("🌸", exercise.rating.intimacy)} </span>
-            <span> {ratingRenderer("🕑", exercise.rating.effort)} </span>
-          </div>
-        </animated.div>
-      <animated.div className={'contentContainer padded stack border back'} style={flipCard ? rotatePropsReverse : {}}>
+    <motion.div className="tarotCard" onClick={(e) => setflipCard(!flipCard)}>
+      <motion.div
+        className={"contentContainer padded stack border back"}
+        initial={{ rotateY: 180 }}
+        animate={{ rotateY: 360 }}
+        transition={{ ease: "linear" }}
+      >
         <div> card back</div>
-      </animated.div>
+      </motion.div>
+      <motion.div
+        className={"contentContainer padded stack border front"}
+        initial={{ rotateY: 0 }}
+        animate={{ rotateY: 180 }}
+        transition={{ ease: "linear" }}
+      >
+        <div className="caption"> {exercise.name} </div>
+        <div className="flex-1"> {exercise.text} </div>
+        <div className="align-end horizontal-stack fullWidth">
+          <span> {ratingRenderer("🌸", exercise.rating.intimacy)} </span>
+          <span> {ratingRenderer("🕑", exercise.rating.effort)} </span>
+        </div>
+      </motion.div>
+
       {share && shareButton(exercise)}
-    </animated.div>
+    </motion.div>
   );
 };
 
